5ab9f80a8cb9456aa8cb7bd4114b34297d65d116
SUDARK <2963502563@qq.com>
2025-05-18 14:37:29 +0800

    sudark

---
+++ src/main/java/sudark2/Sudark/myLord/MyLord.java
@@ -0,0 +1,17 @@
+package sudark2.Sudark.myLord;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MyLord extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/sudark2/Sudark/myLord/PlayerTame.java
@@ -0,0 +1,4 @@
+package sudark2.Sudark.myLord;
+
+public class PlayerTame {
+}
